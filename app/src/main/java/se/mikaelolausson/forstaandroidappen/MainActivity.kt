b36bbc584611f5b7f2837ed02452c5653a57b68a
cfmicke <147425805+cfmicke@users.noreply.github.com>
2023-10-23 19:51:47 +0200

    Update MainActivity.kt

--- app/src/main/java/se/mikaelolausson/forstaandroidappen/MainActivity.kt
+++ app/src/main/java/se/mikaelolausson/forstaandroidappen/MainActivity.kt
@@ -19,7 +19,7 @@ class MainActivity : ComponentActivity() {
             ForstaAndroidAppenTheme {
                 // A surface container using the 'background' color from the theme
                 Surface(modifier = Modifier.fillMaxSize(), color = MaterialTheme.colorScheme.background) {
-                    Greeting("Mikael")
+                    Greeting("Mikael Olausson")
                 }
             }
         }
