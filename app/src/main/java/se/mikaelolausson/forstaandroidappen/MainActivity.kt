04ff38ebce06b3c45457f51f7b13c0e1a9a5aeb4
Mikael <mikael.u.olausson@gmail.com>
2023-10-23 19:39:41 +0200

    start

---
+++ app/src/main/java/se/mikaelolausson/forstaandroidappen/MainActivity.kt
@@ -0,0 +1,43 @@
+package se.mikaelolausson.forstaandroidappen
+
+import android.os.Bundle
+import androidx.activity.ComponentActivity
+import androidx.activity.compose.setContent
+import androidx.compose.foundation.layout.fillMaxSize
+import androidx.compose.material3.MaterialTheme
+import androidx.compose.material3.Surface
+import androidx.compose.material3.Text
+import androidx.compose.runtime.Composable
+import androidx.compose.ui.Modifier
+import androidx.compose.ui.tooling.preview.Preview
+import se.mikaelolausson.forstaandroidappen.ui.theme.ForstaAndroidAppenTheme
+
+class MainActivity : ComponentActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContent {
+            ForstaAndroidAppenTheme {
+                // A surface container using the 'background' color from the theme
+                Surface(modifier = Modifier.fillMaxSize(), color = MaterialTheme.colorScheme.background) {
+                    Greeting("Mikael")
+                }
+            }
+        }
+    }
+}
+
+@Composable
+fun Greeting(name: String, modifier: Modifier = Modifier) {
+    Text(
+            text = "Hej $name!",
+            modifier = modifier
+    )
+}
+
+@Preview(showBackground = true)
+@Composable
+fun GreetingPreview() {
+    ForstaAndroidAppenTheme {
+        Greeting("Android")
+    }
+}
